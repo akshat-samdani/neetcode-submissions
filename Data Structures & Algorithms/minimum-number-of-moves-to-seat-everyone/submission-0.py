697833176958ce68732f997fb73e3d46bc469e89
Akshat Samdani <akshatsamdani23@gmail.com>
2026-07-06 22:35:16 +0530

    Add: minimum-number-of-moves-to-seat-everyone - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-moves-to-seat-everyone/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        # diff min so sort both and take absolute diff
+        seats.sort()
+        students.sort()
+        minMoves = 0
+        for i in range(len(seats)):
+            minMoves += abs(seats[i] - students[i])
+
+        return minMoves
+        
