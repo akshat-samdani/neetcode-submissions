011423ea861bb39299d7ea6b5b6823134b456364
Akshat Samdani <akshatsamdani23@gmail.com>
2026-07-08 23:27:32 +0530

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexed_nums = [(num , i) for i, num in enumerate(nums)]
+
+        indexed_nums.sort(key = lambda x: x[0])
+
+        i , j = 0, len(nums) - 1
+
+        while i < j:
+            currSum = indexed_nums[i][0] + indexed_nums[j][0]
+            if currSum == target:
+                return [min(indexed_nums[i][1], indexed_nums[j][1]),
+                        max(indexed_nums[i][1], indexed_nums[j][1])]
+            elif currSum < target:
+                i += 1
+            else:
+                j -= 1
+            
+        
