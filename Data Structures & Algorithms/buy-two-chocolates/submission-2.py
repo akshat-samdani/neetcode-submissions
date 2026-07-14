1291094d00bc14e457cb39ec0dc190236e8e6bd4
Akshat Samdani <akshatsamdani23@gmail.com>
2026-07-14 22:19:08 +0530

    Add: buy-two-chocolates - submission-2

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        # Using sort
+        # prices.sort()
+        # first, second = prices[0], prices[1]
+        # ans = money - (first + second) 
+        # return ans if ans >= 0 else money
+
+        # Using greedy
+        # Maintain min1 and min2
+
+        min1 = min2 = money
+
+        for p in prices:
+            if p < min1:
+                min2 = min1 # min2 will be the second minimum
+                min1 = p
+            elif p < min2:
+                min2 = p
+        ans = money - (min1 + min2) 
+        return ans if ans >= 0 else money
+        
+
+
+
+        
