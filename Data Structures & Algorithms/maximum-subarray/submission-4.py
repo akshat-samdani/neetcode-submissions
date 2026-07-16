5cbd489d28e9ae0a94a318c20196a19f4dfa44a0
Akshat Samdani <akshatsamdani23@gmail.com>
2026-07-16 23:05:48 +0530

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        # Brute force - O(n*n) & O(1)
+        # Try all possible subarrays
+
+        # maxSum = float("-inf")
+        # n = len(nums)
+        # for i in range(n):
+        #     currSum = 0
+        #     for j in range(i, n):
+        #         currSum += nums[j]
+        #         maxSum = max(currSum, maxSum)
+            
+        # return maxSum
+
+        # Optimal - O(n) & O(1) - Greedy/ Kadan's alog
+        currSum = 0
+        maxSum = nums[0]
+
+        for num in nums:
+            if currSum < 0: currSum = 0
+            currSum += num
+            maxSum = max(maxSum, currSum)
+        return maxSum
+
+
+
+        
