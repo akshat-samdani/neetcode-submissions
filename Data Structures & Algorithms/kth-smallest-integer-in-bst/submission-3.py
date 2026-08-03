29c582c32e1e4ea4d7539fcfa75265c235be39b5
Akshat Samdani <akshatsamdani23@gmail.com>
2026-08-03 23:45:04 +0530

    Add: kth-smallest-integer-in-bst - submission-3

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-3.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # order = []
+        # def inorder(node):
+        #     if not node or len(order) == k:
+        #         return 
+        #     inorder(node.left)
+        #     order.append(node.val)
+        #     inorder(node.right)
+        # inorder(root)
+        
+        # return order[k - 1]
+
+        ans = None
+
+        def inorder(node):
+            # Recursive calls must modify the outer k and ans, not local copies.
+            # Prevent Python from creating new local variables named k and ans.
+            nonlocal ans, k
+            if not node or ans:
+                return 
+            inorder(node.left)
+            k -= 1
+            if k == 0: 
+                ans = node.val
+                return
+            inorder(node.right)
+
+        inorder(root)
+        return ans
+
+        # Or use [] list as they are pass by reference instead of pass by value
+        ans = []
+        count = [k]
+
+        def inorder(node):
+        
+            if not node or count[0] == 0:
+                return 
+            inorder(node.left)
+            count[0] -= 1
+            if count[0] == 0: 
+                ans.append(node.val)
+                return
+            inorder(node.right)
+
+        inorder(root)
+        return ans[0]
+        
